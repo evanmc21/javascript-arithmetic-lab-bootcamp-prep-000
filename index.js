3bdc3b97fa302987e5d50f85a5c4c00df910c3ef
Evan Candler <emcandler21@gmail.com>
2017-11-14 17:18:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function divide(a,b) {
   number = a / b;
   return number;
 }
-function increment(a) {
+function inc(a) {
   number = a + 1
 }
-function decrement(a) {
+function dec(a) {
   number = a - 1
 }
